d20e68bd6c043c08151654bdba267d98c4c5ae9d
Tomek Dubrownik <t.dubrownik@gmail.com>
2012-11-04 10:28:39 +0100

    not sure if this is even consistent atm ^^;

--- config.h
+++ config.h
@@ -3,30 +3,40 @@
 /* appearance */
 //static const char font[]            = "-*-terminus-medium-r-*-*-16-*-*-*-*-*-*-*";
 //static const char font[]            = "-*-nimbus sans l-medium-r-normal-*-14-*-*-*-*-*-*-*";
-static const char font[]            = "Mono 10";
+//static const char font[]            = "DejaVu 9";
+#include "push.c"
+static const char font[]            = "Terminus (TTF) 9";
 static const char normbordercolor[] = "#444444";
 static const char normbgcolor[]     = "#222222";
 static const char normfgcolor[]     = "#bbbbbb";
 static const char selbordercolor[]  = "#bbbbbb";
-static const char selbgcolor[]      = "#700000";
+static const char selbgcolor[]      = "#333333";
 static const char selfgcolor[]      = "#eeeeee";
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const Bool showbar           = True;     /* False means no bar */
 static const Bool topbar            = True;     /* False means bottom bar */
+static const Bool viewontag         = True;
 
-/* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "q", "w", "e", };
+/* tagging -- see tags.h */
 
 static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
-//	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
+	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
 //	{ "Firefox",  NULL,       NULL,       1 << 8,       False,       -1 },
-	{ "Google-chrome",   NULL,       NULL,       1 << 9,       False,       -1 },
         { "Amarok",          NULL,       NULL,       1 << 10,      False,       -1 },    
-	{ "Pidgin",          NULL,       NULL,       1 << 11,      False,       -1 },
-	{ "HTOP",            NULL,       NULL,       1 << 8,       False,       -1 },
-	{ "NCMPCPP",         NULL,       NULL,       1 << 10,      False,       -1 },
+	{ "Pidgin",          NULL,       NULL,       1 << 11,      False,        0 },
+	{ "HTOP",            NULL,       NULL,       1 << 8,       False,        0 },
+	{ "NCMPCPP",         NULL,       NULL,       1 << 10,      False,        0 },
+	{ "MPlayer",         NULL,       NULL,       0,            True,         1 },
+	{ "CONSOLE",         NULL,       NULL,       1 << 13,      False,        0 },
+	{ "stalonetray",     NULL,       NULL,       1 << 14,      False,        0 },
+	{ "Google-chrome",   "google-chrome",       NULL,       0,            False,       -1 },
+	{ "Google-chrome",   "gmail.com",           NULL,       1 << 9,       False,        0 },
+	{ "Thunderbird",     "Mail",     NULL,       1 << 9,       False,        0 },
+	{ "Google-chrome",   "calendar.google.com", NULL,       1 << 6,       False,        0 },
+	{ "Google-chrome",   "www.wunderlist.com",  NULL,       1 << 7,       False,        0 },
+	{ "Google-chrome",   "web.tweetdeck.com",   NULL,       1 << 12,      False,        0 },
 };
 
 /* layout(s) */
@@ -39,6 +49,7 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "###",      gaplessgrid },
 };
 
 /* pertag-static */
@@ -54,9 +65,11 @@ static TagConfig pertag[LENGTH(tags) + 1] = {
         DEFAULT_TAG, /* 7 */
         DEFAULT_TAG, /* 8 */
         DEFAULT_TAG, /* 9 */
-        { 2,            1,            1,               0.55 }, /* q */
+        { 0,            1,            1,               0.60 }, /* q */
         DEFAULT_TAG, /* w */
         { 0,            1,            1,               0.8  }, /* e */
+        DEFAULT_TAG, /* r */
+        { 3,            1,            1,               0.5  }, /* ~ */
 };
 
 /* key definitions */
@@ -73,18 +86,25 @@ static TagConfig pertag[LENGTH(tags) + 1] = {
 
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "uxterm", NULL };
-static const char *lockcmd[]  = { "slock", NULL };
+static const char *termcmd[]  = { "evilvte", NULL };
+static const char *browsercmd[]  = { "google-chrome", NULL };
+static const char *focuscmd[]  = { "/home/tomek/Programs/xtools/dmenu_focus", NULL };
+static const char *fastmenucmd[]  = { "/home/tomek/Programs/xtools/m", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
 	{ ALT,                          XK_t,      spawn,          {.v = termcmd } },
+//	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_space,  spawn,          {.v = focuscmd } },
+	{ MODKEY|ControlMask,           XK_space,  spawn,          {.v = fastmenucmd } },
+	{ MODKEY,                       XK_n,      spawn,          {.v = browsercmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ ALT,                          XK_Tab,    focusstack,     {.i = +1 } },
-	{ ALT|ShiftMask,                XK_Tab,    focusstack,     {.i = -1 } },
+	{ ALT,                          XK_Tab,    swapfocus,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+        { MODKEY|ShiftMask,             XK_j,      pushdown,       {0} },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+        { MODKEY|ShiftMask,             XK_k,      pushup,         {0} },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
@@ -95,7 +115,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY|ShiftMask,             XK_f,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
@@ -108,7 +129,7 @@ static Key keys[] = {
         { MODKEY,                       0x1008ff11,spawn,          SHCMD("mpc prev") },
         { 0,                            0x1008ff13,spawn,          SHCMD("volume inc") },
         { MODKEY,                       0x1008ff13,spawn,          SHCMD("mpc next") },
-        { ALT|ControlMask,              XK_l,      spawn,          {.v = lockcmd } },        
+        { ALT|ControlMask,              XK_l,      spawn,          SHCMD("lock") },        
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -121,6 +142,11 @@ static Key keys[] = {
 	TAGKEYS(                        XK_q,                      9)
 	TAGKEYS(                        XK_w,                      10)
 	TAGKEYS(                        XK_e,                      11)
+	TAGKEYS(                        XK_r,                      12)
+	TAGKEYS(                        XK_grave,                  13)
+
+        { MODKEY,                       XK_backslash, toggleview,  {.ui = 1 << 14 } },
+
 	{ ALT|ShiftMask,                XK_q,      quit,           {0} },
 };
 
---
+++ tags.h
@@ -0,0 +1,3 @@
+/* split out due to pertag <-> gaplessgrid collision */
+static int hidden = 1;
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "q", "w", "e", "r", "~", "_" };
